f4e224734ad6b569f8a846e9536975bc9716dfdb
n1nvhil <n1vnhil@outlook.com>
2025-05-28 17:26:50 +0800

    add ollama invoke

--- src/main/java/org/n1vnhil/awesomes/awesome/ai/agent/controller/TestController.java
+++ src/main/java/org/n1vnhil/awesomes/awesome/ai/agent/controller/TestController.java
@@ -1,6 +1,8 @@
 package org.n1vnhil.awesomes.awesome.ai.agent.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @Autowired
-    private ChatClient dashScopeChatClient;
+    private ChatClient dashscopeChatClient;
+
+    @Autowired
+    private ChatModel ollamaChatModel;
 
     @GetMapping
     public String test() {
         return "ok";
     }
 
-    @GetMapping("/chat")
-    public String chat() {
-        return dashScopeChatClient.prompt("你好吗？").call().content();
+    @GetMapping("/dashscope/chat")
+    public String dashscopeChat() {
+        return dashscopeChatClient.prompt("你好吗？").call().content();
     }
+
+    @GetMapping("/ollama/chat")
+    public String ollamaChat() {
+        return ollamaChatModel.call(new Prompt("你好吗？"))
+                .getResult()
+                .getOutput()
+                .getText();
+    }
+
+
 
 }
