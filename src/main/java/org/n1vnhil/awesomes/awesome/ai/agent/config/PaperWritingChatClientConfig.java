84f1f618403e979ac5abc93ebc08d5547f0e2f86
n1vnhil <n1vnhil@outlook.com>
2025-05-29 17:43:08 +0800

    add file based ChatMemory

--- src/main/java/org/n1vnhil/awesomes/awesome/ai/agent/config/PaperWritingChatClientConfig.java
+++ src/main/java/org/n1vnhil/awesomes/awesome/ai/agent/config/PaperWritingChatClientConfig.java
@@ -6,6 +6,7 @@ import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.n1vnhil.awesomes.awesome.ai.agent.advisors.LogAdvisor;
 import org.n1vnhil.awesomes.awesome.ai.agent.advisors.Re2Advisor;
+import org.n1vnhil.awesomes.awesome.ai.agent.chatmemory.FileBasedChatMemory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
@@ -21,10 +22,11 @@ public class PaperWritingChatClientConfig {
 
     @Bean
     public ChatClient dashScopeChatClient() {
+        String saveDir = System.getProperty("user.dir") + "/chat-memory";
         return ChatClient.builder(new DashScopeChatModel(new DashScopeApi(System.getenv("BAILIAN_API_KEY"))))
                 .defaultSystem(SYSTEM_PROMPT)
                 .defaultAdvisors(
-                        new MessageChatMemoryAdvisor(new InMemoryChatMemory()),
+                        new MessageChatMemoryAdvisor(new FileBasedChatMemory(saveDir)),
                         new LogAdvisor(),
                         new Re2Advisor()
                 )
